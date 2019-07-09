c8281ceea24e67a60acdcf6a76b70b1a9fc211ef
Owor Yoakim <owor327@gmail.com>
2019-07-09 20:53:55 +0300

    Integrated Blankon theme

---
+++ resources/views/default.blade.php
@@ -0,0 +1,59 @@
+@extends('layout-sidebar-default')
+@section('content')
+    <!-- Start header content -->
+    <div class="header-content">
+        <h2><i class="fa fa-file-o"></i>Blank Page <span>blank page sample</span></h2>
+        <div class="breadcrumb-wrapper hidden-xs">
+            <span class="label">You are here:</span>
+            <ol class="breadcrumb">
+                <li>
+                    <i class="fa fa-home"></i>
+                    <a href="dashboard.html">Dashboard</a>
+                    <i class="fa fa-angle-right"></i>
+                </li>
+                <li>
+                    <a href="#">Layouts</a>
+                    <i class="fa fa-angle-right"></i>
+                </li>
+                <li class="active">Blank Page</li>
+            </ol>
+        </div>
+    </div><!-- /.header-content -->
+    <!--/ End header content -->
+
+    <!-- Start body content -->
+    <div class="body-content animated fadeIn" style="min-height: 600px;">
+
+        <div class="row">
+            <div class="col-md-12">
+
+                <div class="panel rounded shadow">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Page Blank</h3>
+                    </div><!-- /.panel-heading -->
+                    <div class="panel-body">
+                        page blank
+                    </div><!-- /.panel-body -->
+                </div><!-- /.panel -->
+
+            </div>
+        </div>
+
+    </div><!-- /.body-content -->
+    <!--/ End body content -->
+
+    <!-- Start footer content -->
+    <footer class="footer-content">
+        @include('footer')
+    </footer><!-- /.footer-content -->
+    <!--/ End footer content -->
+
+    </section><!-- /#page-content -->
+    <!--/ END PAGE CONTENT -->
+@endsection
+@section('scripts')
+    <script>
+
+
+    </script>
+@endsection
---
+++ resources/views/sidebar.blade.php
@@ -0,0 +1,102 @@
+<!-- Start left navigation - profile shortcut -->
+<div class="sidebar-content">
+    <div class="media">
+        <a class="pull-left has-notif avatar" href="page-profile.html">
+            <img src="http://img.djavaui.com/?create=50x50,4888E1?f=ffffff" alt="admin">
+            <i class="online"></i>
+        </a>
+        <div class="media-body">
+            <h4 class="media-heading">Hello, <span>Lee</span></h4>
+            <small>Web Designer</small>
+        </div>
+    </div>
+</div><!-- /.sidebar-content -->
+<!--/ End left navigation -  profile shortcut -->
+
+<!-- Start left navigation - menu -->
+<ul class="sidebar-menu">
+
+    <!-- Start navigation -->
+    <li>
+        <a href="#">
+            <span class="icon"><i class="fa fa-list"></i></span>
+            <span class="text">Level 1</span>
+            <span class="label label-danger pull-right">New</span>
+        </a>
+    </li>
+    <li class="submenu">
+        <a href="javascript:void(0);">
+            <span class="icon"><i class="fa fa-list"></i></span>
+            <span class="text">Level 1</span>
+            <span class="arrow"></span>
+        </a>
+        <ul>
+            <li><a href="#">Level 2</a></li>
+            <li><a href="#">Level 2</a></li>
+            <li><a href="#">Level 2</a></li>
+        </ul>
+    </li>
+    <li class="submenu">
+        <a href="javascript:void(0);">
+            <span class="icon"><i class="fa fa-list"></i></span>
+            <span class="text">Level 1</span>
+            <span class="plus"></span>
+        </a>
+        <ul>
+            <li><a href="#">Level 2</a></li>
+            <li><a href="#">Level 2</a></li>
+            <li><a href="#">Level 2</a></li>
+        </ul>
+    </li>
+    <!--/ End navigation -->
+
+    <!-- Start category apps -->
+    <li class="sidebar-category">
+        <span>CATEGORY</span>
+        <span class="pull-right"><i class="fa fa-trophy"></i></span>
+    </li>
+    <!--/ End category apps -->
+
+    <!-- Start development - layouts -->
+    <li class="submenu active">
+        <a href="javascript:void(0);">
+            <span class="icon"><i class="fa fa-columns"></i></span>
+            <span class="text">Layouts</span>
+            <span class="plus"></span>
+            <span class="selected"></span>
+        </a>
+        <ul>
+            <li><a href="layout-blank.html">Blank Page</a></li>
+            <li><a href="layout-boxed.html">Boxed Page</a></li>
+            <li><a href="layout-header-fixed.html">Header Fixed Page</a></li>
+            <li><a href="layout-sidebar-fixed.html">Sidebar Fixed Page</a></li>
+            <li><a href="layout-sidebar-minimize.html">Sidebar Minimize Page</a></li>
+            <li class="active"><a href="layout-sidebar-default.html">Sidebar Default Page</a></li>
+            <li><a href="layout-sidebar-box.html">Sidebar Box Page</a></li>
+            <li><a href="layout-sidebar-rounded.html">Sidebar Rounded Page</a></li>
+            <li><a href="layout-sidebar-circle.html">Sidebar Circle Page</a></li>
+            <li><a href="layout-footer-fixed.html">Footer Fixed Page</a></li>
+        </ul>
+    </li>
+    <!--/ End development - layouts -->
+
+    <!-- Start documentation - api documentation -->
+    <li>
+        <a href="../../../documentation/admin/html/live-preview-documentation.html" target="_blank">
+            <span class="icon"><i class="fa fa-book"></i></span>
+            <span class="text">API Documentation</span>
+        </a>
+    </li>
+    <!--/ End documentation - api documentation -->
+
+</ul><!-- /.sidebar-menu -->
+<!--/ End left navigation - menu -->
+
+<!-- Start left navigation - footer -->
+<div class="sidebar-footer hidden-xs hidden-sm hidden-md">
+    <a id="setting" class="pull-left" href="javascript:void(0);" data-toggle="tooltip" data-placement="top" data-title="Setting"><i class="fa fa-cog"></i></a>
+    <a id="fullscreen" class="pull-left" href="javascript:void(0);" data-toggle="tooltip" data-placement="top" data-title="Fullscreen"><i class="fa fa-desktop"></i></a>
+    <a id="lock-screen" data-url="page-signin.html" class="pull-left" href="javascript:void(0);" data-toggle="tooltip" data-placement="top" data-title="Lock Screen"><i class="fa fa-lock"></i></a>
+    <a id="logout" data-url="page-lock-screen.html" class="pull-left" href="javascript:void(0);" data-toggle="tooltip" data-placement="top" data-title="Logout"><i class="fa fa-power-off"></i></a>
+</div><!-- /.sidebar-footer -->
+<!--/ End left navigation - footer -->
