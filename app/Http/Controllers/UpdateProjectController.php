47efae3f3a6ac54852b79cb17c959edc6d2d02c3
Owor Yoakim <owor327@gmail.com>
2019-07-31 16:55:26 +0300

    Refactor update project code

--- app/Http/Controllers/UpdateProjectController.php
+++ app/Http/Controllers/UpdateProjectController.php
@@ -13,19 +13,25 @@ class UpdateProjectController extends Controller
 {
     public function update(UpdateProjectRequest $request, Project $project)
     {
-        // check user
-        $user = Sentinel::getUser();
-        if ($user->id != $project->user_id)
+        try
         {
-            return response()->json('This project does not belong to you!', Response::HTTP_FORBIDDEN);
+            // check user
+            $user = Sentinel::getUser();
+            if ($user->id != $project->user_id)
+            {
+                return response()->json('This project does not belong to you!', Response::HTTP_FORBIDDEN);
+            }
+
+            $project->update([
+                'title' => $request->get('title'),
+                'description' => $request->get('description'),
+                'image' => $request->get('image'),
+            ]);
+
+            return response()->json('Project updated!');
+        } catch (Exception $ex)
+        {
+            return response()->json($ex->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
         }
-
-        $project->update([
-            'title' => $request->get('title'),
-            'description' => $request->get('description'),
-            'image' => $request->get('image'),
-        ]);
-
-        return response()->json($project);
     }
 }
